6e91a3dc6d2846ed2ae05555e414ad55c8b53551
Sunny <83127107+RajSunny2011@users.noreply.github.com>
2025-01-19 09:27:31 +0530

    Modified Bill.java

--- Assignment1/Bill.java
+++ Assignment1/Bill.java
@@ -1,36 +1,24 @@
 import java.util.Scanner;
 public class Bill {
+    public static int calculateBill(int totalUnits){
+        int charge = 0, currentBracket = 0;
+        int chargeBrackets[] = {5,7,10,15};
+        while (totalUnits >= 100 && currentBracket < 3){
+            charge += chargeBrackets[currentBracket] * 100;
+            totalUnits -= 100;
+            currentBracket += 1;
+        }
+        charge += totalUnits>0 ? (chargeBrackets[currentBracket] * totalUnits) : 0;
+        return charge;
+    }
+
     public static void main(String[] args) {
-        int totalUnits, charge = 0, remainingUnits;
+        int totalUnits, charge;
         try (Scanner in = new Scanner(System.in)){
             System.out.print("Enter total Units: ");
             totalUnits = in.nextInt();
         }
-        remainingUnits = totalUnits;
-        if (remainingUnits<101 && remainingUnits != 0){
-            charge += remainingUnits*5;
-            remainingUnits = 0;
-        }else if (remainingUnits>0){
-            charge += 500;
-            remainingUnits -= 100;
-        }
-        if (remainingUnits<101 && remainingUnits != 0){
-            charge += remainingUnits*7;
-            remainingUnits = 0;
-        }else if (remainingUnits>0){
-            charge += 700;
-            remainingUnits -= 100;
-        }
-        if (remainingUnits<101 && remainingUnits != 0){
-            charge += remainingUnits*10;
-            remainingUnits = 0;
-        }else if (remainingUnits>0){
-            charge += 1000;
-            remainingUnits -= 100;
-        }
-        if (remainingUnits != 0){
-            charge += remainingUnits*15;
-        }
+        charge = calculateBill(totalUnits);
         System.out.println("Total Bill: " + charge);
     }
 }
