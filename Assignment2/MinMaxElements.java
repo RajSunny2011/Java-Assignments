205e4a188805a213dbbfe54632885a20f9766c7e
Sunny <83127107+RajSunny2011@users.noreply.github.com>
2025-01-24 19:31:57 +0530

    new file: Assignment2/MinMaxElements.java

---
+++ Assignment2/MinMaxElements.java
@@ -0,0 +1,12 @@
+
+public class MinMaxElements {
+    public static void main(String[] args) {
+        int numbers[] = {1,2,4,4,6,-1};
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for (int i : numbers){
+            max = (max < i) ? i : max;
+            min = (min > i) ? i : min;
+        }
+        System.out.println("Max: "+max+" Min: "+min);
+    }
+}
