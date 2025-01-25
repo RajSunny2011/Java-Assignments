22d8938215d4b8c7731d46fbf4a4fb89d22facc6
Sunny <83127107+RajSunny2011@users.noreply.github.com>
2025-01-25 09:00:13 +0530

    modified Assignment1/question1Pattern.java

--- Assignment1/question1Pattern.java
+++ Assignment1/question1Pattern.java
@@ -1,13 +1,8 @@
 public class question1Pattern {
     public static void main(String[] args) {
         for (int i = 5; i > 0; i--){
-            for (int j = 5; j >= i; j--){
-                System.out.print(" ");
-            }
-            for (int k = 1; k<=i; k++){
-                System.out.print("*");
-            }
-            System.out.print("\n");
+            System.out.print(" ".repeat(5-i));
+            System.out.println("*".repeat(i));
         }
     }
 }
