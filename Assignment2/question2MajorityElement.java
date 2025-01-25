8fa02d40b00c7121b515f61a73d395835ed37066
Sunny <83127107+RajSunny2011@users.noreply.github.com>
2025-01-25 09:18:13 +0530

    modified: Assignment2/question2MajorityElement.java

--- Assignment2/question2MajorityElement.java
+++ Assignment2/question2MajorityElement.java
@@ -2,17 +2,17 @@ import java.util.HashMap;
 public class question2MajorityElement {
     public static void main(String[] args) {
         int numbers[] = {1,2,4,6,4,4,4};
-        HashMap<Integer, Integer> occurenceHashMap = new HashMap<>();
+        HashMap<Integer, Integer> frequencyHashMap = new HashMap<>();
         for (int i : numbers) {
-            if (occurenceHashMap.containsKey(i)){
-                occurenceHashMap.put(i, (occurenceHashMap.get(i)+1));
+            if (frequencyHashMap.containsKey(i)){
+                frequencyHashMap.put(i, (frequencyHashMap.get(i)+1));
             }
             else{
-                occurenceHashMap.put(i, 1);
+                frequencyHashMap.put(i, 1);
             }
         }
-        for (int i : occurenceHashMap.keySet()) {
-            if (occurenceHashMap.get(i)>(numbers.length/2)){
+        for (int i : frequencyHashMap.keySet()) {
+            if (frequencyHashMap.get(i)>(numbers.length/2)){
                 System.out.println(i);
                 break;
             }
