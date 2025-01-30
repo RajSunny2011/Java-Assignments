dc9ca460e47b043c3264e4155dd62e6d3aa7dc9c
Sunny <83127107+RajSunny2011@users.noreply.github.com>
2025-01-30 17:14:11 +0530

    new file: Experiment4/question1Book.java

---
+++ Experiment4/question1Book.java
@@ -0,0 +1,38 @@
+public class question1Book {
+    public static void main(String[] args) {
+        book book1 = new book();
+        book book2 = new book("My Book", "John Doe", 2023);
+
+        System.out.println("\nDetails of first book:");
+        book1.displayBook();
+
+        System.out.println("\nDetails of second book:");
+        book2.displayBook();
+    }
+}
+
+class book{
+    private String title;
+    private String author;
+    private int year;
+    public book() {
+        this("Untitled", "Unkown Author");
+    }
+    public book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.year = -1;
+    }
+    public book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    public void displayBook() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        if (year != -1) {
+            System.out.println("Published in Year: " + year);
+        }
+    }
+}
