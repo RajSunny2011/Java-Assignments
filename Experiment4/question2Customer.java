b2fb278ab99834304cae024c11d0ef0f99b18749
Sunny <83127107+RajSunny2011@users.noreply.github.com>
2025-01-30 17:25:46 +0530

    Add Experiment 4 file and quesiton 2

---
+++ Experiment4/question2Customer.java
@@ -0,0 +1,29 @@
+public class question2Customer{
+    public static void main(String[] args) {
+        customer c1 = new customer();
+        c1.addBalance(50.0);
+        c1.deductBalance(30.0);
+    }
+}
+class customer{
+    private double balance;
+    protected void showBalance() {
+        System.out.println("Current Balance: $" + String.format("%.2f", balance));
+    }
+    public void addBalance(double amount) {
+        balance += amount;
+        showDetails();
+    }
+    public void deductBalance(double amount) {
+        if (balance < amount) {
+            System.out.println("Insufficient funds for withdrawal.");
+            return;
+        }
+        balance -= amount;
+        showDetails();
+    }
+    private void showDetails() {
+        System.out.println("\nCustomer Details");
+        System.out.printf("\tBalance: %.2f", balance);
+    }
+}
