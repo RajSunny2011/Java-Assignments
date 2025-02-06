23b487b05a2eb860d47d612f6225db709dd3bf18
Sunny <83127107+RajSunny2011@users.noreply.github.com>
2025-02-06 17:12:49 +0530

    Exp5\Q1 done

---
+++ Experiment 5/question4Car.java
@@ -0,0 +1,46 @@
+abstract class Vehicle {
+    private final String make;
+    private final String model;
+
+    public Vehicle(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+    public abstract void startEngine();
+    public abstract void stopEngine();
+    public void serviceInfo() {
+        System.out.println("Basic servicing instructions: Check oil, tire pressure, and brakes.");
+    }
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
+
+class Car extends Vehicle {
+    public Car(String make, String model) {
+        super(make, model);
+    }
+    @Override
+    public void startEngine() {
+        System.out.println("The car engine is starting...");
+    }
+    @Override
+    public void stopEngine() {
+        System.out.println("The car engine is stopping...");
+    }
+}
+
+public class question4Car{
+    public static void main(String[] args) {
+        Car myCar = new Car("Tesla", "Model S");
+        myCar.startEngine();
+        myCar.stopEngine();
+        myCar.serviceInfo();
+        System.out.println("Car make: " + myCar.getMake());
+        System.out.println("Car model: " + myCar.getModel());
+    }
+}
